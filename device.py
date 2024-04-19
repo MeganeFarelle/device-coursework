3b29103a4516ff5b0862e6dc3b43f7eb3d0aef99
MeganeFarelle <98145263+MeganeFarelle@users.noreply.github.com>
2024-04-19 05:58:32 +0400

    Update device.py

--- device.py
+++ device.py
@@ -21,7 +21,7 @@ def upload_images(bucket_name, image_paths):
             unique_name = str(uuid.uuid4())  # Generate a random unique name
             new_file_name = f"{unique_name}_{file_name}"  # Append the unique name to the file name
             s3_client.upload_file(image_path, bucket_name, new_file_name)
-            print(f"Uploaded {new_file_name} to {bucket_name}.")
+            print(f"Uploaded {file_name} to {bucket_name}.")
             time.sleep(30)  # Delay between uploads
     except Exception as e:
         print(f"Error uploading image {file_name} to {bucket_name}: {str(e)}")
