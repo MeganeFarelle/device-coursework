ab8e4f842db967ba721f7185ca5c4ba4001fe4c9
MeganeFarelle <98145263+MeganeFarelle@users.noreply.github.com>
2024-04-19 12:00:54 +0400

    Update device.py

--- device.py
+++ device.py
@@ -1,6 +1,8 @@
 import argparse
 import boto3
 import os
+import time
+import uuid
 
 # Setting up AWS S3 client
 s3_client = boto3.client('s3')
@@ -16,12 +18,13 @@ def upload_images(bucket_name, image_paths):
     try:
         for image_path in image_paths:
             file_name = os.path.basename(image_path)
-            s3_client.upload_file(image_path, bucket_name, file_name)
-            print(f"{file_name} successfully uploaded to {bucket_name}.")  # Print success message
+            unique_name = str(uuid.uuid4())  # Generate a random unique name
+            new_file_name = f"{unique_name}_{file_name}"  # Append the unique name to the file name
+            s3_client.upload_file(image_path, bucket_name, new_file_name)
+            print(f"Uploaded {new_file_name} to {bucket_name}.")
+            time.sleep(30)  # Delay between uploads
     except Exception as e:
-        print(f"Error uploading images to {bucket_name}: {str(e)}")
-    else:
-        print("All images uploaded successfully to S3.")
+        print(f"Error uploading image: {str(e)}")
 
 def get_image_paths(directory):
     """
